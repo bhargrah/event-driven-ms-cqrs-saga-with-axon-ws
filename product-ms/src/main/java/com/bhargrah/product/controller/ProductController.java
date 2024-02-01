f5ccb7bb1a797df1567f5d00e8459a3b7b3c8ee5
Rahul Bhargava <rahulbhargava21@gmail.com>
2024-02-01 19:14:28 +0530

    Upgrading services and adding axon server boot

--- product-ms/src/main/java/com/bhargrah/product/controller/ProductController.java
+++ product-ms/src/main/java/com/bhargrah/product/controller/ProductController.java
@@ -1,19 +1,27 @@
 package com.bhargrah.product.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
     @PostMapping
     public String createProduct(){
+
         return "HTTP POST Handler";
     }
 
     @GetMapping
     public  String getProduct(){
-        return "HTTP GET Handler";
+        return "HTTP GET Handler : " + environment.getProperty("local.server.port");
     }
 
     @PutMapping
